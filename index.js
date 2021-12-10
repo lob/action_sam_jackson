7b5b081ff8c43273e0ad0e922750290bb390cea7
Shariq Torres <shariq.torres@lob.com>
2021-12-10 15:38:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ async function runMain(){
         var url = octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments')
         await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
             issue_number: github.context.payload.issue.id,
-            owner: github.context.payload.issue.owner.login,
+            owner: github.context.payload.owner.login,
             repo: github.context.payload.issue.repo.name,
             body: core.getInput('message')
         })
 
     } catch( err ) {
-        console.log(github.context.payload.issue.comments_url)
         console.log("There was an error executing the action: " + err)
         core.setFailed(err.message)
     }
