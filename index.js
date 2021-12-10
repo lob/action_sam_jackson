39b7778c7bad1b019a7ec7aa1cc835284f3b024f
Shariq Torres <shariq.torres@lob.com>
2021-12-10 15:42:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const core = require("@actions/core")
 const {Octokit } = require("octokit")
 async function runMain(){
     try {
+        console.log(github.context)
+
         const octokit = new Octokit(github.token)
         var url = octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments')
         await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
