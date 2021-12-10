00750bb6598856c0b096e3ac9de749431fb77d11
Shariq Torres <shariq.torres@lob.com>
2021-12-10 13:31:29 -0800

    updated

--- index.js
+++ index.js
@@ -2,4 +2,15 @@ const github = require("@actions/github")
 
 const core = require("@actions/core")
 
-console.log(github.context.payload)
+const {Octokit, App} = require("octokit")
+
+try {
+    const octokit = new Octokit(github.token)
+    await octokit.request(github.context.payload.issue.comments_url, {
+        body: inputs.message
+    })
+
+} catch( err ) {
+    console.log("There was an error executing the action: " + err)
+    core.setFailed(err.message)
+}
