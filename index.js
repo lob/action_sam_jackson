dc7c6b6bd9ae140275b562cc316b0026f6426e20
Shariq Torres <shariq.torres@lob.com>
2021-12-11 06:18:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const github = require("@actions/github")
 
 const core = require("@actions/core")
 
-const {Octokit } = require("octokit")
+const { Octokit } = require("@octokit/action");
 async function runMain(){
     try {
         console.log(github.context.payload)
-        const octokit = new github.getOctokit({auth: core.getInput('token')})
+        const octokit = new Octokit({auth: core.getInput('token')})
         console.log({
             issue_number: github.context.payload.issue.number,
             owner: github.context.payload.repository.owner.login,
             repo: github.context.payload.repository.name,
             body: core.getInput('message')
         })
+        const { data } = await octokit.request("POST /repos/{owner}/{repo}/issues", {
+            owner,
+            repo,
+            title: "My test issue",
+          });
+        console.log(data)
         let res = await octokit.rest.issues.createComment({
             issue_number: github.context.payload.issue.number,
             owner: github.context.payload.repository.owner.login,
