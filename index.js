f256f29424ac3bcf39994e955071d51b2dff2338
Shariq Torres <shariq.torres@lob.com>
2021-12-11 05:44:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,20 @@ async function runMain(){
     try {
         console.log(core.getInput('token'))
         const octokit = new Octokit({auth: core.getInput('token')})
-        const { data } = await octokit.request("/user");
-        // let res = await octokit.rest.issues.createComment({
-        //     issue_number: github.context.payload.issue.id,
-        //     owner: github.context.payload.repository.owner.login,
-        //     repo: github.context.payload.repository.name,
-        //     body: core.getInput('message')
-        // })
-        // console.log(res)
-        console.log(data)
+        console.log({
+            issue_number: github.context.payload.issue.id,
+            owner: github.context.payload.repository.owner.login,
+            repo: github.context.payload.repository.name,
+            body: core.getInput('message')
+        })
+        let res = await octokit.rest.issues.createComment({
+            issue_number: github.context.payload.issue.id,
+            owner: github.context.payload.repository.owner.login,
+            repo: github.context.payload.repository.name,
+            body: core.getInput('message')
+        })
+        console.log(res)
+        // console.log(data)
 
     } catch( err ) {
         console.log("There was an error executing the action: " + err)
