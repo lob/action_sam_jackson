0cd77c86b77fff3e8c488db28287a5e59cd1a103
Shariq Torres <shariq.torres@lob.com>
2021-12-10 17:47:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const core = require("@actions/core")
 const {Octokit } = require("octokit")
 async function runMain(){
     try {
-        core.setOutput("sdfsdf")
-
-        const octokit = new Octokit(github.token)
-        await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
-            issue_number: github.context.payload.issue,
-            owner: github.context.payload.owner.login,
-            repo: github.context.payload.issue.repo.name,
-            body: core.getInput('message')
-        })
+        console.log(github.context.payload)
+        // const octokit = new Octokit(github.token)
+        // await octokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+        //     issue_number: github.context.payload.issue.id,
+        //     owner: github.context.payload.owner.login,
+        //     repo: github.context.payload.issue.repo.name,
+        //     body: core.getInput('message')
+        // })
 
     } catch( err ) {
         console.log("There was an error executing the action: " + err)
